6d4f243a9a0f7d450650f7fba7209f4d07aedc54
abraham <abraham@corehubdev.corehub>
2019-10-29 15:16:16 +0530

    1.0-update

--- filebook.go
+++ filebook.go
@@ -127,6 +127,8 @@ type FileType struct {
 	Name     string
 }
 
+var fbA *admin.Admin
+
 func main() {
 	initLog()
 	dB, _ := gorm.Open("sqlite3", "dbfb.db")
@@ -145,12 +147,16 @@ func main() {
 		&Sender{},
 	)
 
-	fbA := admin.New(&admin.AdminConfig{DB: dB, SiteName: "File Book"})
+	fbA = admin.New(&admin.AdminConfig{DB: dB, SiteName: "File Book"})
 	fbA.AssetFS.PrependPath(filepath.Join(utils.AppRoot, "views"))
 
-	loadMasters(fbA)
-	loadRes("User", fbA)
-	loadRes("Inward", fbA)
+	loadMasters()
+	loadResUser()
+	loadResInward()
+	loadResSeat()
+	loadResDept()
+	loadResBranch()
+	loadResOrg()
 
 	mux := http.NewServeMux()
 	fbA.MountTo("/admin", mux)
@@ -168,7 +174,7 @@ func initLog() {
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
 }
 
-func loadMasters(fbA *admin.Admin) {
+func loadMasters() {
 	confFMgt := admin.Config{
 		Menu: []string{
 			"File Management",
@@ -189,379 +195,325 @@ func loadMasters(fbA *admin.Admin) {
 	fbA.AddResource(&Branch{}, &confAdmn)
 }
 
-func loadRes(nR string, fbA *admin.Admin) {
-	switch nR {
-	case "User":
-		userRes := fbA.GetResource(nR)
-		genderTypes := []string{
-			"Male",
-			"Female",
-			"Other",
-			"Unfilled",
-		}
-
-		configGenderTypes := &admin.SelectOneConfig{
-			Collection: genderTypes,
-		}
-
-		userRes.IndexAttrs("-Password")
-		userRes.Meta(&admin.Meta{
-			Name:      "Password",
-			FieldName: "password",
-			Type:      "password",
-			Valuer:    func(interface{}, *qor.Context) interface{} { return "" },
-			Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-				if newPassword := utils.ToString(metaValue.Value); newPassword != "" {
-					//pWSHA := strToSHA256(newPassword)
-					record.(*User).Password = string(newPassword)
-				}
-			},
-		})
-		userRes.Meta(&admin.Meta{
-			Name:   "Gender",
-			Config: configGenderTypes,
-		})
-
-		userRes.Meta(&admin.Meta{
-			Name: "Dob",
-			Type: "date",
-		})
-
-		attrsUser := [][]string{
-			{"Seat", "seat"},
-			{"Dept.", "department"},
-			{"Org.", "organization"},
-			{"Department", "department"},
-			{"Organization", "organization"},
-		}
-
-		for _, aU := range attrsUser {
-			valuer := func(interface{}, *qor.Context) interface{} { return "" }
-			setter := func(interface{}, *resource.MetaValue, *qor.Context) {}
-			switch aU[1] {
-			case "seat":
-				valuer = func(r interface{}, c *qor.Context) interface{} {
-					return getName(fbA, aU[1], r.(*User).SeatID)
-				}
-				setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
-					if newValue := utils.ToString(mV.Value); newValue != "" {
-						r.(*User).SeatID = getID(fbA, aU[1], newValue)
-					}
-				}
-			case "department":
-				valuer = func(r interface{}, c *qor.Context) interface{} {
-					return getName(fbA, aU[1], r.(*User).DepartmentID)
-				}
-				setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
-					if newValue := utils.ToString(mV.Value); newValue != "" {
-						r.(*User).DepartmentID = getID(fbA, aU[1], newValue)
-					}
-				}
-			case "organization":
-				valuer = func(r interface{}, c *qor.Context) interface{} {
-					return getName(fbA, aU[1], r.(*User).OrganizationID)
-				}
-				setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
-					if newValue := utils.ToString(mV.Value); newValue != "" {
-						r.(*User).OrganizationID = getID(fbA, aU[1], newValue)
-					}
-				}
-			}
-			userRes.Meta(&admin.Meta{
-				Name:      aU[0],
-				FieldName: aU[1],
-				Type:      "string",
-				Valuer:    valuer,
-				Setter:    setter,
-			})
-		}
-		/*
-			user.Meta(&admin.Meta{
-				Name:      "Seat",
-				FieldName: "seat",
-				Type:      "string",
-				Valuer:    func(r interface{}, c *qor.Context) interface{} { return getName(r.(*User).SeatID) },
-				Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-					if newValue := utils.ToString(metaValue.Value); newValue != "" {
-						record.(*User).SeatID = getID(newValue)
-					}
-				},
-			})
-
-			user.Meta(&admin.Meta{
-				Name:      "Dept.",
-				FieldName: "department",
-				Type:      "string",
-				Valuer:    func(r interface{}, c *qor.Context) interface{} { return getName(r.(*User).DepartmentID) },
-				Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-					if newValue := utils.ToString(metaValue.Value); newValue != "" {
-						record.(*User).DepartmentID = getID(newValue)
-					}
-				},
-			})
-
-			user.Meta(&admin.Meta{
-				Name:      "Org.",
-				FieldName: "organization",
-				Type:      "string",
-				Valuer:    func(r interface{}, c *qor.Context) interface{} { return getName(r.(*User).OrganizationID) },
-				Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-					if newValue := utils.ToString(metaValue.Value); newValue != "" {
-						record.(*User).OrganizationID = getID(newValue)
-					}
-				},
-			})
-
-			user.Meta(&admin.Meta{
-				Name:      "Department",
-				FieldName: "department",
-				Type:      "string",
-				Valuer:    func(r interface{}, c *qor.Context) interface{} { return getName(r.(*User).DepartmentID) },
-				Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-					if newValue := utils.ToString(metaValue.Value); newValue != "" {
-						record.(*User).DepartmentID = getID(newValue)
-					}
-				},
-			})
-
-			user.Meta(&admin.Meta{
-				Name:      "Organization",
-				FieldName: "organization",
-				Type:      "string",
-				Valuer:    func(r interface{}, c *qor.Context) interface{} { return getName(r.(*User).OrganizationID) },
-				Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-					if newValue := utils.ToString(metaValue.Value); newValue != "" {
-						record.(*User).OrganizationID = getID(newValue)
-					}
-				},
-			})
-		*/
-
-		rowsUserDetailsNE := [][]string{
-			{"Name", "Phone"},
-			{"Email", "Password"},
-			{"Dob", "Gender"},
-			{"Addresses"},
-			{"Seat"},
-			{"Department"},
-			{"Organization"},
-		}
-
-		rowsUserDetailsI := [][]string{
-			{"Name"},
-			{"Email", "Phone"},
-			{"Dob", "Gender"},
-			{"Addresses"},
-			{"Seat"},
-			{"Dept."},
-			{"Org."},
-		}
-
-		sectionUserDetailsNE := &admin.Section{
-			Rows: rowsUserDetailsNE,
-		}
-
-		sectionUserDetailsI := &admin.Section{
-			Rows: rowsUserDetailsI,
-		}
-
-		userRes.NewAttrs(
-			sectionUserDetailsNE,
-		)
-
-		userRes.EditAttrs(
-			sectionUserDetailsNE,
-		)
-
-		userRes.IndexAttrs(
-			sectionUserDetailsI,
-		)
-		userRes.SearchAttrs(
-			"ID",
-			"Name",
-			"Email",
-			"Phone",
-			"Addresses.Address",
-			"Addresses.Pincode",
-		)
-		/*
-			for _, gT := range genderTypes {
-				user.Scope(&admin.Scope{
-					Name:    gT,
-					Group:   "Gender",
-					Handler: handlerScope("Gender", gT),
-				})
-			}
-		*/
-
-	case "Inward":
-		inward := fbA.GetResource(nR)
-
-		typesInward := []string{
-			"Letter",
-			"Application",
-			"Tender",
-			"Invitation",
-		}
-
-		modesInward := []string{
-			"By Hand",
-			"Tele Call",
-			"Email",
-			"Web Enquiry",
-		}
-
-		statusesInward := []string{
-			"Received",
-			"Opened",
-			"Processed",
-			"Rejected",
-		}
-
-		typesSender := []string{
-			"Individual",
-			"Department",
-			"Organization",
-		}
-
-		rowsMenuInwardDetails := [][]string{
-			{"Type", "Mode"},
-			{"Date"},
-			{"Remarks"},
-		}
-
-		rowsMenuSenderDetails := [][]string{
-			{"Type", "Name"},
-			{"Email", "Phone"},
-			{"Address"},
-		}
-
-		sectionInwardDetails := &admin.Section{
-			Title: "Inward Details",
-			Rows:  rowsMenuInwardDetails,
-		}
-
-		sectionSenderDetails := &admin.Section{
-			Rows: rowsMenuSenderDetails,
-		}
-
-		inward.Meta(&admin.Meta{
-			Name: "Type",
-			Config: &admin.SelectOneConfig{
-				Collection: typesInward,
-			},
-		})
-
-		inward.Meta(&admin.Meta{
-			Name: "Mode",
-			Config: &admin.SelectOneConfig{
-				Collection: modesInward,
-			},
-		})
-
-		inward.Meta(&admin.Meta{
-			Name: "Status",
-			Config: &admin.SelectOneConfig{
-				Collection: statusesInward,
-			},
-		})
-
-		inward.EditAttrs(
-			"Title",
-			"Sender",
-			sectionInwardDetails,
-			"Files",
-		)
-
-		inward.NewAttrs(
-			"Title",
-			"Sender",
-			sectionInwardDetails,
-			"Status",
-			"Files",
-		)
-
-		inward.SearchAttrs(
-			"ID",
-			"Title",
-			"Sender.Name",
-		)
-
-		metaSender := inward.Meta(&admin.Meta{
-			Name: "Sender",
-		})
-
-		sndrRes := metaSender.Resource
-
-		sndrRes.EditAttrs(
-			sectionSenderDetails,
-		)
-
-		sndrRes.NewAttrs(
-			sectionSenderDetails,
-		)
-
-		sndrRes.Meta(&admin.Meta{
-			Name: "Type",
-			Config: &admin.SelectOneConfig{
-				Collection: typesSender,
-			},
-		})
-
-		sndrRes.Meta(&admin.Meta{
-			Name:      "Address",
-			FieldName: "address",
-			Type:      "text",
-			Setter:    fnSetter("Address"),
-		})
-
-		inward.Meta(&admin.Meta{
-			Name:      "Remarks",
-			FieldName: "remarks",
-			Type:      "text",
-			Setter:    fnSetter("Remarks"),
-		})
-
-		inward.IndexAttrs(
-			"-Files",
-			"-Remarks",
-		)
-
-		fieldsScope := [][]string{
-			typesInward,
-			modesInward,
-			statusesInward,
-		}
-
-		namesScope := []string{
-			"Type",
-			"Mode",
-			"Status",
-		}
-
-		for i, n := range namesScope {
-			for _, f := range fieldsScope[i] {
-				inward.Scope(&admin.Scope{
-					Name:    f,
-					Group:   n,
-					Handler: handlerScope(n, f),
-				})
-			}
-		}
-
-		inward.Config.IconName = "IconInward"
+func loadResUser() {
+	userRes := fbA.GetResource("User")
+	genderTypes := []string{
+		"Male",
+		"Female",
+		"Other",
+		"Unfilled",
 	}
+
+	configGenderTypes := &admin.SelectOneConfig{
+		Collection: genderTypes,
+	}
+
+	userRes.IndexAttrs("-Password")
+
+	userRes.Meta(&admin.Meta{
+		Name:      "Password",
+		FieldName: "password",
+		Type:      "password",
+		Valuer:    func(interface{}, *qor.Context) interface{} { return "" },
+		Setter: func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
+			if newPassword := utils.ToString(metaValue.Value); newPassword != "" {
+				//pWSHA := strToSHA256(newPassword)
+				record.(*User).Password = string(newPassword)
+			}
+		},
+	})
+
+	userRes.Meta(&admin.Meta{
+		Name:   "Gender",
+		Config: configGenderTypes,
+	})
+
+	userRes.Meta(&admin.Meta{
+		Name: "Dob",
+		Type: "date",
+	})
+
+	attrsUser := [][]string{
+		{"Seat", "seat"},
+		{"Dept.", "department"},
+		{"Org.", "organization"},
+		{"Department", "department"},
+		{"Organization", "organization"},
+	}
+
+	valuer := func(interface{}, *qor.Context) interface{} { return "" }
+	setter := func(interface{}, *resource.MetaValue, *qor.Context) {}
+
+	for _, aU := range attrsUser {
+		switch aU[1] {
+
+		case "seat":
+			seat := Seat{}
+			valuer = func(r interface{}, c *qor.Context) interface{} {
+				fbA.DB.Where("id = ?", r.(*User).SeatID).First(&seat)
+				return seat.Name
+			}
+			setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
+				if newValue := utils.ToString(mV.Value); newValue != "" {
+					fbA.DB.Where("name = ?", newValue).First(&seat)
+					r.(*User).SeatID = seat.ID
+				}
+			}
+
+		case "department":
+			dept := Department{}
+			valuer = func(r interface{}, c *qor.Context) interface{} {
+				fbA.DB.Where("id = ?", r.(*User).DepartmentID).First(&dept)
+				return dept.Name
+			}
+			setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
+				if newValue := utils.ToString(mV.Value); newValue != "" {
+					fbA.DB.Where("name = ?", newValue).First(&dept)
+					r.(*User).DepartmentID = dept.ID
+				}
+			}
+
+		case "organization":
+			org := Organization{}
+			valuer = func(r interface{}, c *qor.Context) interface{} {
+				fbA.DB.Where("id = ?", r.(*User).OrganizationID).First(&org)
+				return org.Name
+			}
+			setter = func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
+				if newValue := utils.ToString(mV.Value); newValue != "" {
+					fbA.DB.Where("name = ?", newValue).First(&org)
+					r.(*User).OrganizationID = org.ID
+				}
+			}
+		}
+
+		userRes.Meta(&admin.Meta{
+			Name:      aU[0],
+			FieldName: aU[1],
+			Type:      "string",
+			Valuer:    valuer,
+			Setter:    setter,
+		})
+	}
+
+	rowsUserDetailsNE := [][]string{
+		{"Name", "Phone"},
+		{"Email", "Password"},
+		{"Dob", "Gender"},
+		{"Addresses"},
+		{"Seat"},
+		{"Department"},
+		{"Organization"},
+	}
+
+	rowsUserDetailsI := [][]string{
+		{"Name"},
+		{"Email", "Phone"},
+		{"Dob", "Gender"},
+		{"Addresses"},
+		{"Seat"},
+		{"Dept."},
+		{"Org."},
+	}
+
+	sectionUserDetailsNE := &admin.Section{
+		Rows: rowsUserDetailsNE,
+	}
+
+	sectionUserDetailsI := &admin.Section{
+		Rows: rowsUserDetailsI,
+	}
+
+	userRes.NewAttrs(
+		sectionUserDetailsNE,
+	)
+
+	userRes.EditAttrs(
+		sectionUserDetailsNE,
+	)
+
+	userRes.IndexAttrs(
+		sectionUserDetailsI,
+	)
+	userRes.SearchAttrs(
+		"ID",
+		"Name",
+		"Email",
+		"Phone",
+		"Addresses.Address",
+		"Addresses.Pincode",
+	)
+	/*
+		for _, gT := range genderTypes {
+			user.Scope(&admin.Scope{
+				Name:    gT,
+				Group:   "Gender",
+				Handler: handlerScope("Gender", gT),
+			})
+		}
+	*/
 }
 
-func fnSetter(s string) func(interface{}, *resource.MetaValue, *qor.Context) {
-	return func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
-		if nV := utils.ToString(mV.Value); nV != "" {
-			switch s {
-			case "Address":
+func loadResInward() {
+	resInward := fbA.GetResource("Inward")
+
+	typesInward := []string{
+		"Letter",
+		"Application",
+		"Tender",
+		"Invitation",
+	}
+
+	modesInward := []string{
+		"By Hand",
+		"Tele Call",
+		"Email",
+		"Web Enquiry",
+	}
+
+	statusesInward := []string{
+		"Received",
+		"Opened",
+		"Processed",
+		"Rejected",
+	}
+
+	typesSender := []string{
+		"Individual",
+		"Department",
+		"Organization",
+	}
+
+	rowsMenuInwardDetails := [][]string{
+		{"Type", "Mode"},
+		{"Date"},
+		{"Remarks"},
+	}
+
+	rowsMenuSenderDetails := [][]string{
+		{"Type", "Name"},
+		{"Email", "Phone"},
+		{"Address"},
+	}
+
+	sectionInwardDetails := &admin.Section{
+		Title: "Inward Details",
+		Rows:  rowsMenuInwardDetails,
+	}
+
+	sectionSenderDetails := &admin.Section{
+		Rows: rowsMenuSenderDetails,
+	}
+
+	resInward.Meta(&admin.Meta{
+		Name: "Type",
+		Config: &admin.SelectOneConfig{
+			Collection: typesInward,
+		},
+	})
+
+	resInward.Meta(&admin.Meta{
+		Name: "Mode",
+		Config: &admin.SelectOneConfig{
+			Collection: modesInward,
+		},
+	})
+
+	resInward.Meta(&admin.Meta{
+		Name: "Status",
+		Config: &admin.SelectOneConfig{
+			Collection: statusesInward,
+		},
+	})
+
+	resInward.EditAttrs(
+		"Title",
+		"Sender",
+		sectionInwardDetails,
+		"Files",
+	)
+
+	resInward.NewAttrs(
+		"Title",
+		"Sender",
+		sectionInwardDetails,
+		"Status",
+		"Files",
+	)
+
+	resInward.SearchAttrs(
+		"ID",
+		"Title",
+		"Sender.Name",
+	)
+
+	metaSender := resInward.Meta(&admin.Meta{
+		Name: "Sender",
+	})
+
+	sndrRes := metaSender.Resource
+
+	sndrRes.EditAttrs(
+		sectionSenderDetails,
+	)
+
+	sndrRes.NewAttrs(
+		sectionSenderDetails,
+	)
+
+	sndrRes.Meta(&admin.Meta{
+		Name: "Type",
+		Config: &admin.SelectOneConfig{
+			Collection: typesSender,
+		},
+	})
+
+	sndrRes.Meta(&admin.Meta{
+		Name:      "Address",
+		FieldName: "address",
+		Type:      "text",
+		Setter: func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
+			if nV := utils.ToString(mV.Value); nV != "" {
 				r.(*Sender).Address = string(nV)
-			case "Remarks":
+			}
+		},
+	})
+
+	resInward.Meta(&admin.Meta{
+		Name:      "Remarks",
+		FieldName: "remarks",
+		Type:      "text",
+		Setter: func(r interface{}, mV *resource.MetaValue, c *qor.Context) {
+			if nV := utils.ToString(mV.Value); nV != "" {
 				r.(*Inward).Remarks = string(nV)
 			}
+		},
+	})
+
+	resInward.IndexAttrs(
+		"-Files",
+		"-Remarks",
+	)
+
+	fieldsScope := [][]string{
+		typesInward,
+		modesInward,
+		statusesInward,
+	}
+
+	namesScope := []string{
+		"Type",
+		"Mode",
+		"Status",
+	}
+
+	for i, n := range namesScope {
+		for _, f := range fieldsScope[i] {
+			resInward.Scope(&admin.Scope{
+				Name:    f,
+				Group:   n,
+				Handler: handlerScope(n, f),
+			})
 		}
 	}
 }
@@ -572,63 +524,26 @@ func handlerScope(n string, f string) func(*gorm.DB, *qor.Context) *gorm.DB {
 	}
 }
 
+func loadResSeat() {
+	//resSeat := fbA.GetResource("Seat")
+
+}
+
+func loadResDept() {
+
+}
+
+func loadResBranch() {
+
+}
+
+func loadResOrg() {
+
+}
+
 func strToSHA256(str string) []byte {
 	strSHA := sha1.New()
 	strSHA.Write([]byte(str))
 	strSHAHexStr := hex.EncodeToString(strSHA.Sum(nil))
 	return []byte(strSHAHexStr)
-}
-
-func getID(fbA *admin.Admin, typeRes string, name string) uint {
-	var id uint
-	switch typeRes {
-	case "user":
-		res := User{}
-		fbA.DB.Where("name = ?", name).First(&res)
-		id = res.ID
-	case "seat":
-		res := Seat{}
-		fbA.DB.Where("name = ?", name).First(&res)
-		id = res.ID
-	case "department":
-		res := Department{}
-		fbA.DB.Where("name = ?", name).First(&res)
-		id = res.ID
-	case "branch":
-		res := Branch{}
-		fbA.DB.Where("name = ?", name).First(&res)
-		id = res.ID
-	case "organization":
-		res := Organization{}
-		fbA.DB.Where("name = ?", name).First(&res)
-		id = res.ID
-	}
-	return id
-}
-
-func getName(fbA *admin.Admin, typeRes string, id uint) string {
-	var name string
-	switch typeRes {
-	case "user":
-		res := User{}
-		fbA.DB.Where("id = ?", id).First(&res)
-		name = res.Name
-	case "seat":
-		res := Seat{}
-		fbA.DB.Where("id = ?", id).First(&res)
-		name = res.Name
-	case "department":
-		res := Department{}
-		fbA.DB.Where("id = ?", id).First(&res)
-		name = res.Name
-	case "branch":
-		res := Branch{}
-		fbA.DB.Where("id = ?", id).First(&res)
-		name = res.Name
-	case "organization":
-		res := Organization{}
-		fbA.DB.Where("id = ?", id).First(&res)
-		name = res.Name
-	}
-	return name
 }
